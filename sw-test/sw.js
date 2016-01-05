fd0b0cef5ed35e88e244db13493bf3d9c6148534
yihaiyang [易海洋] <yihaiyang@xiaomi.com>
2016-01-05 11:07:56 +0800

    remove cache

--- sw-test/sw.js
+++ sw-test/sw.js
@@ -1,6 +1,6 @@
 this.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v6').then(function(cache) {
+    caches.open('v1').then(function(cache) {
       return cache.addAll([
         '/sw-test/',
         '/sw-test/index.html',
@@ -23,7 +23,7 @@ this.addEventListener('fetch', function(event) {
     return fetch(event.request);
   }).then(function(r) {
     response = r;
-    caches.open('v6').then(function(cache) {
+    caches.open('v1').then(function(cache) {
       cache.put(event.request, response);
     });
     return response.clone();
@@ -32,19 +32,19 @@ this.addEventListener('fetch', function(event) {
   }));
 });
 
-// this.addEventListener('activate', function(event) {
-//   var cacheList = ['v6'];
+this.addEventListener('activate', function(event) {
+  var cacheList = ['v1'];
 
-//   event.waitUntil(
-//     caches.keys(function(cacheNames) {
-//       return Promise.all(
-//         cacheNames.map(function(cacheName) {
-//           if (cacheList.indexOf(cacheName) == -1) {
-//             return caches.delete(cacheName)
-//           }
-//         });
-//       );
-//     });
-//   );
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheList.indexOf(cacheName) == -1) {
+            return caches.delete(cacheName)
+          }
+        });
+      );
+    });
+  );
   
-// });
+});
