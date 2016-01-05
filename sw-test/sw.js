356b61b4826ba697fc35b7918f86f678bc6fb313
yihaiyang [易海洋] <yihaiyang@xiaomi.com>
2016-01-05 11:02:03 +0800

    cancel remove cache

--- sw-test/sw.js
+++ sw-test/sw.js
@@ -32,19 +32,19 @@ this.addEventListener('fetch', function(event) {
   }));
 });
 
-this.addEventListener('activate', function(event) {
-  var cacheList = ['v6'];
+// this.addEventListener('activate', function(event) {
+//   var cacheList = ['v6'];
 
-  event.waitUntil(
-    caches.keys(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheList.indexOf(cacheName) == -1) {
-            return caches.delete(cacheName)
-          }
-        });
-      );
-    });
-  );
+//   event.waitUntil(
+//     caches.keys(function(cacheNames) {
+//       return Promise.all(
+//         cacheNames.map(function(cacheName) {
+//           if (cacheList.indexOf(cacheName) == -1) {
+//             return caches.delete(cacheName)
+//           }
+//         });
+//       );
+//     });
+//   );
   
-});
+// });
